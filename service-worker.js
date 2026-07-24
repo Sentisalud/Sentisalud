ec493253de275fac262950b4e16a6a1d047d3d78
Sentisalud <torreserika976@gmail.com>
2026-07-23 22:49:58 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    - Cualquier origen externo (mapas CARTO, Leaflet CDN, datos.gov.co, Supabase):
      NO se intercepta ni se cachea — va directo a la red.
    Sube el número de versión para forzar actualización del shell. */
-var VERSION = 'sentisalud-v9';
+var VERSION = 'sentisalud-v12';
 var SHELL = [
   './',
   './index.html',
